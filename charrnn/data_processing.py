6ed8d159bb2caceb0d634d0451e9e5fa41b9b10c
Peter Roelants <peter.roelants+git@gmail.com>
2016-10-09 10:36:02 +0100

    data processing utf-8

--- charrnn/data_processing.py
+++ charrnn/data_processing.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from __future__ import division
 
 import re
+import codecs
 
 filepath = 'war_and_peace.txt'
 out_file = 'wap.txt'
@@ -12,7 +13,7 @@ NEW_LINE_IN_PARAGRAPH_REGEX = re.compile(r'(\S)\r\n(\S)')
 MULTIPLE_NEWLINES_REGEX = re.compile(r'(\r\n)(\r\n)+')
 R_NEWLINE = re.compile(r'\r')
 
-with open(filepath, 'r') as f_input:
+with codecs.open(filepath, encoding='utf-8', mode='r') as f_input:
     book_str = f_input.read()
 
 
@@ -20,5 +21,5 @@ book_str = NEW_LINE_IN_PARAGRAPH_REGEX.sub('\g<1> \g<2>', book_str)
 book_str = MULTIPLE_NEWLINES_REGEX.sub('\n\n', book_str)
 book_str = R_NEWLINE.sub('', book_str)
 
-with open(out_file, 'w') as f_output:
+with codecs.open(out_file, encoding='utf-8', mode='w')as f_output:
     f_output.write(book_str)
